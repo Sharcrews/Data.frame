7ae3cbb4c526069ac0fb4bdec788e78d3d57c623
Sharcrews <70011495+Sharcrews@users.noreply.github.com>
2022-01-30 19:45:31 -0500

    Commit

---
+++ PollDataframe.R
@@ -0,0 +1,8 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hilary", "Bernie")
+
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS <- c(12, 75, 43, 19, 1, 21, 19)
+
+Poll <- data.frame(Name, ABC, CBS)
+Poll
